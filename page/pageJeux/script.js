0ff663559d8000897e9159f76ca591c711f62bea
clément4444 <clement.fiquet0@gmail.com>
2024-10-15 15:17:35 +0200

    ajoute modification

---
+++ page/pageJeux/script.js
@@ -0,0 +1,7 @@
+//récuper la base de donner
+
+//choisir 8 profil aléatoir (stoker dans une autre liste ou il a que les 8 profil)
+
+//récuperer les 16 case via un queryselltorall
+
+//dans une boucle for crée un écouteur
