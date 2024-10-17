642b9d4b9b090534ba49d81d3ddc758abbccb3c3
Fabien <fafa.verhulst@gmail.com>
2024-10-17 18:36:27 +0200

    installation gestion badges

--- page/pageProfil/gestionBadge.js
+++ page/pageProfil/gestionBadge.js
@@ -12,3 +12,29 @@ if(!donner.utilisateurs[donner.profilSelect].badges.profil100){
     documentBadgeImg100Profil.src = "./public/point interrogation.png";
 }
 
+//badge 10ème compte
+const documentBadgeTexte10Compte = document.querySelector("#badgeText10Compte");
+const documentBadgeImg10Compte = document.querySelector("#badgeImg10Compte");
+if(!donner.utilisateurs[donner.profilSelect].badges.compte10){
+    console.log("ok");
+    documentBadgeTexte10Compte.classList.add('invisible');
+    documentBadgeImg10Compte.src = "./public/point interrogation.png";
+}
+
+//badge bouton secret
+const documentBadgeTexteBoutonSecret = document.querySelector("#badgeTextBoutonSecret");
+const documentBadgeImgBoutonSecret = document.querySelector("#badgeImgBoutonSecret");
+if(!donner.utilisateurs[donner.profilSelect].badges.boutonSecrt){
+    console.log("ok");
+    documentBadgeTexteBoutonSecret.classList.add('invisible');
+    documentBadgeImgBoutonSecret.src = "./public/point interrogation.png";
+}
+
+//Jeux des Wilders
+const documentBadgeTexte100JeuxWilders = document.querySelector("#badgeText100JeuxWilders");
+const documentBadgeImg100JeuxWilders = document.querySelector("#badgeImg100JeuxWilders");
+if(!donner.utilisateurs[donner.profilSelect].badges.jeux){
+    console.log("ok");
+    documentBadgeTexte100JeuxWilders.classList.add('invisible');
+    documentBadgeImg100JeuxWilders.src = "./public/point interrogation.png";
+}
