97f825c56728f91552c4eebf6c7531ed4006046c
Philippevde <contact@van-der-eecken.eu>
2024-10-17 18:01:55 +0200

    Ajout darkmode

--- darkMode.js
+++ darkMode.js
@@ -1,2 +1,31 @@
 //ici
 console.log("ok");
+
+// darkMode.js
+document.addEventListener('DOMContentLoaded', () => {
+    const darkmodButton = document.getElementById('darkmod');
+    const imageDarkmod = document.getElementById('imageDarkmod');
+
+    // Vérifie le mode actuel et applique-le
+    if (localStorage.getItem('theme') === 'dark') {
+        document.body.classList.add('dark-mode');
+        imageDarkmod.src = './commun/image/lune.png';
+    } else {
+        document.body.classList.add('light-mode');
+        imageDarkmod.src = './commun/image/soleil.png';
+    }
+
+    darkmodButton.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        document.body.classList.toggle('light-mode');
+
+        // Change l'image du bouton
+        if (document.body.classList.contains('dark-mode')) {
+            imageDarkmod.src = './commun/image/lune.png';
+            localStorage.setItem('theme', 'dark');
+        } else {
+            imageDarkmod.src = './commun/image/soleil.png';
+            localStorage.setItem('theme', 'light');
+        }
+    });
+});
